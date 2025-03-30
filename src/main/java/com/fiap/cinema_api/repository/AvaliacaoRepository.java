da84d23ae2b0de8f3aff4384ac804aedb057dc00
samueldamasceno <samueldamasceno.vip@gmail.com>
2025-03-30 15:05:01 -0300

    camada repository implementada com métodos personalizados

--- src/main/java/com/fiap/cinema_api/repository/AvaliacaoRepository.java
+++ src/main/java/com/fiap/cinema_api/repository/AvaliacaoRepository.java
@@ -3,6 +3,11 @@ package com.fiap.cinema_api.repository;
 import com.fiap.cinema_api.model.Avaliacao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AvaliacaoRepository extends JpaRepository<Avaliacao, Long> {
+
+    List<Avaliacao> findByFilmeId(Long filmeId);
+    List<Avaliacao> findByUsuarioId(Long usuarioId);
 
 }
--- src/main/java/com/fiap/cinema_api/repository/FilmeRepository.java
+++ src/main/java/com/fiap/cinema_api/repository/FilmeRepository.java
@@ -3,6 +3,10 @@ package com.fiap.cinema_api.repository;
 import com.fiap.cinema_api.model.Filme;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FilmeRepository extends JpaRepository<Filme, Long> {
+
+    List<Filme> findByTituloContainingIgnoreCase(String titulo);
 
 }

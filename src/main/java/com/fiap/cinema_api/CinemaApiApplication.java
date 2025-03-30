4cdc90639802e28453cc2042fbd371c2a7d55724
samueldamasceno <samueldamasceno.vip@gmail.com>
2025-03-30 14:35:44 -0300

    projeto iniciado com spring initializr

---
+++ src/main/java/com/fiap/cinema_api/CinemaApiApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.cinema_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/cinema_api/CinemaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.cinema_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

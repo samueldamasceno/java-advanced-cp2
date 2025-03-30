577bb77947cef9101926c24688784def3882cc75
samueldamasceno <samueldamasceno.vip@gmail.com>
2025-03-30 15:08:43 -0300

    filmeservice feito com crud

---
+++ src/main/java/com/fiap/cinema_api/service/FilmeService.java
@@ -0,0 +1,45 @@
+package com.fiap.cinema_api.service;
+
+import com.fiap.cinema_api.model.Filme;
+import com.fiap.cinema_api.repository.FilmeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FilmeService {
+
+    @Autowired
+    private FilmeRepository repository;
+
+    public List<Filme> listarTodos() {
+        return repository.findAll();
+    }
+
+    public Filme buscarPorId(Long id) {
+        return repository.findById(id).orElseThrow(() -> new RuntimeException("Filme não encontrado"));
+    }
+
+    public Filme criar(Filme filme) {
+        return repository.save(filme);
+    }
+
+    public Filme atualizar(Long id, Filme atualizado) {
+        Filme existente = buscarPorId(id);
+        existente.setTitulo(atualizado.getTitulo());
+        existente.setDiretor(atualizado.getDiretor());
+        existente.setGenero(atualizado.getGenero());
+        existente.setAnoLancamento(atualizado.getAnoLancamento());
+        existente.setDuracaoMin(atualizado.getDuracaoMin());
+        return repository.save(existente);
+    }
+
+    public void deletar(Long id) {
+        repository.deleteById(id);
+    }
+
+    public List<Filme> buscarPorTitulo(String titulo) {
+        return repository.findByTituloContainingIgnoreCase(titulo);
+    }
+}
